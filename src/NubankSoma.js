14807d68149eb6318e1cdddcb46cc27b362d57ad
Gabriel Rezende <g@brielrezen.de>
2021-01-31 18:38:44 -0300

    soma do mês aparentemente correta

--- src/NubankSoma.js
+++ src/NubankSoma.js
@@ -1,55 +1,62 @@
-let amountQuery = document.querySelectorAll('.amount')
-console.log(amountQuery)
+console.log('Rodando extensão de soma!');
 
+// Quantas ocorrências tem no último mês?
+
+let diasTotais = document.querySelectorAll('.time')
+let meses =[]
+
+diasTotais.forEach(dia => {
+  if (dia.textContent.length < 7) {
+    meses.push(dia.textContent)
+  }
+});
+
+let mesesLimpo = []
+meses.forEach(frase => {
+  mesesLimpo.push(frase.replace(/\d+/g, '').replace(/\s/g, ''))
+})
+
+let ultimoMes = mesesLimpo[0]
+let numeroUltimoMes = mesesLimpo.lastIndexOf(ultimoMes) // Resultado
+
+// DIV '.amount'
+
+let amountQuery = document.querySelectorAll('.amount')
 let lista = []
 for (let propriedade of amountQuery) {
-  lista.push(propriedade.innerText.replace('R$','').replace('.','').replace(',','.'))
+  if (propriedade.className == "amount") {
+      lista.push(propriedade.innerText.replace('R$','').replace('.','').replace(',','.'))
+  }
 }
-
-console.log(lista)
 
 //
 
 let listaParse = []
 
-lista.forEach(element => {
+lista.slice(0, numeroUltimoMes).forEach(element => {
   listaParse.push(parseFloat(element))
   });
 
-console.log(listaParse)
-
-// Recebido
+// Pagamento de fatura // Bug: Se tiver um "Fatura vence amanhã", mas não tiver um "pagamento recebido", ou se o pagamento recebido for parcial, vai dar mais zica ainda quando for multiplicado por dois na sessão de soma
 
 let recebido = document.querySelectorAll('.payment')
-
-console.log(recebido)
 
 let listaRecebido = []
 
 for (let propriedade of recebido) {
-  listaRecebido.push(propriedade.innerText.replace('Pagamento recebido\nR$ ','').replace('\n11 JAN','').replace('.','').replace(',','.'))
+  listaRecebido.push(propriedade.innerText.replace('Pagamento recebido\nR$ ','').replace('.','').replace(',','.'))
 }
-
-console.log(listaRecebido)
-
-//
-
 let listaRecebidoParse = [];
-
 listaRecebido.forEach( element => {
   listaRecebidoParse.push(parseFloat(element))
 })
 
-console.log(listaRecebidoParse)
-
 // Soma
 
-
 const redutor = (accumulator, currentValue) => accumulator + currentValue;
-
-const somaRecebido = listaRecebidoParse.reduce(redutor)
-
-let somaGasto = listaParse.reduce(redutor) - somaRecebido
+const somaRecebido = listaRecebidoParse.reduce(redutor) * 2 // "* 2" pq se foi pago, também aparece em "Fatura vence amanhã" (consultar bug em "Pagametno de fatura")
+let somaGasto = listaParse.reduce(redutor) - somaRecebido 
+let somaGastoString = somaGasto.toLocaleString('de-DE')
 
 // Exibir
 
@@ -57,20 +64,6 @@ let toolbar = document.querySelector('.toolbar')
 let divTotal  = document.createElement('div');
 divTotal.className += "label"
 divTotal.style.color = "#fff";
-let textoTotal = document.createTextNode(`Total no mês ${somaGasto.toString()}`)
+let textoTotal = document.createTextNode(`Total gasto no mês atual: ${somaGastoString}`)
 divTotal.appendChild(textoTotal);
-
 toolbar.appendChild(divTotal)
-
-
-// Quantos dias tem o mês? // WIP
-
-let diasTotais = document.querySelectorAll('.time').innerText;
-console.log(diasTotais)
-
-let diasLista = []
-for (let propriedade of diasTotais) {
-  diasLista.push(propriedade.innerText.replace('R$','').replace('.','').replace(',','.'))
-}
-
-console.log(diasLista)
