3bb846dfb95d3f286fa142e65ced8e90014cc70c
Gabriel Rezende <g@brielrezen.de>
2021-04-18 14:29:01 -0300

    refatorado do zero

--- src/NubankSoma.js
+++ src/NubankSoma.js
@@ -1,69 +1,47 @@
-console.log('Rodando extensão de soma!');
+// https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
 
-// Quantas ocorrências tem no último mês?
+// Selecionando todas as compras disponíveis:
+let datas2 = Array.from(document.querySelectorAll('.time')).slice(1) 
 
-let diasTotais = document.querySelectorAll('.time')
-let meses =[]
+// Ultimo mês
+let ultimoMesNome = datas2[0].innerText.split(' ')[1] // String do último mês
+ultimoMesNome
 
-diasTotais.forEach(dia => {
-  if (dia.textContent.length < 7) {
-    meses.push(dia.textContent)
+comprasUltimoMes = datas2.filter( dia => { // Todas as DIVs do último mês 
+  return dia.innerText.split(' ')[1] == ultimoMesNome &&
+  (
+    dia.parentElement.classList[2] == "card_present" || dia.parentElement.classList[2] == "card_not_present"
+    )
+  } )
+  
+  comprasUltimoMes
+  
+  // TODO: Separar "card_present" e "card_not_present:
+  // Filtrar:
+  /*
+  exibir:
+  card_not_present
+  card_present
+  
+  não exibir:
+  class="event-card event bill_flow_paid"
+  bill_flow_on_one_day_to_due_date
+  bill_flow_paid
+  payment
+  */
+
+  // Soma de todas as compras do último mês:
+  const convertendoParaNumero = (numString) => {
+    return Number(numString.replace(/,/g, '.').replace(/[^0-9.-]+/g,""))
   }
-});
+  
+  let reducer = (accumulator, div) => accumulator + convertendoParaNumero(div.parentElement.children[3].innerText);
+  let somaComprasUltimoMes = comprasUltimoMes.reduce(reducer, 0)
+let stringSomaUltimoMes = "R$ " + somaComprasUltimoMes.toFixed(2).replace(".", ',')
+stringSomaUltimoMes
 
-let mesesLimpo = []
-meses.forEach(frase => {
-  mesesLimpo.push(frase.replace(/\d+/g, '').replace(/\s/g, ''))
-})
+// DOM:
 
-let ultimoMes = mesesLimpo[0]
-let numeroUltimoMes = mesesLimpo.lastIndexOf(ultimoMes) // Resultado
+const toolbar = document.querySelector(".toolbar");
 
-// DIV '.amount'
-
-let amountQuery = document.querySelectorAll('.amount')
-let lista = []
-for (let propriedade of amountQuery) {
-  if (propriedade.className == "amount") {
-      lista.push(propriedade.innerText.replace('R$','').replace('.','').replace(',','.'))
-  }
-}
-
-//
-
-let listaParse = []
-
-lista.slice(0, numeroUltimoMes).forEach(element => {
-  listaParse.push(parseFloat(element))
-  });
-
-// Pagamento de fatura // Bug: Se tiver um "Fatura vence amanhã", mas não tiver um "pagamento recebido", ou se o pagamento recebido for parcial, vai dar mais zica ainda quando for multiplicado por dois na sessão de soma
-
-let recebido = document.querySelectorAll('.payment')
-
-let listaRecebido = []
-
-for (let propriedade of recebido) {
-  listaRecebido.push(propriedade.innerText.replace('Pagamento recebido\nR$ ','').replace('.','').replace(',','.'))
-}
-let listaRecebidoParse = [];
-listaRecebido.forEach( element => {
-  listaRecebidoParse.push(parseFloat(element))
-})
-
-// Soma
-
-const redutor = (accumulator, currentValue) => accumulator + currentValue;
-const somaRecebido = listaRecebidoParse.reduce(redutor) * 2 // "* 2" pq se foi pago, também aparece em "Fatura vence amanhã" (consultar bug em "Pagametno de fatura")
-let somaGasto = listaParse.reduce(redutor) - somaRecebido 
-let somaGastoString = somaGasto.toLocaleString('de-DE')
-
-// Exibir
-
-let toolbar = document.querySelector('.toolbar')
-let divTotal  = document.createElement('div');
-divTotal.className += "label"
-divTotal.style.color = "#fff";
-let textoTotal = document.createTextNode(`Total gasto no mês atual: ${somaGastoString}`)
-divTotal.appendChild(textoTotal);
-toolbar.appendChild(divTotal)
+toolbar.innerHTML += `<div style="margin-top: 15px; font-size: 1.500rem; line-height: .7rem; color: rgb(255,255,255);">${ultimoMesNome}: ${stringSomaUltimoMes}</div>`
